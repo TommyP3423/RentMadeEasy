024a34cf7a7b4ff7fc498289f1003b580d3b4ebc
TommyP3423 <tompearson3423@gmail.com>
2025-08-15 20:42:59 +1000

    Thanks.vue

---
+++ pages/Thanks.vue
@@ -0,0 +1,7 @@
+<template>
+  <main style="max-width:720px;margin:60px auto;padding:0 20px;text-align:center;">
+    <h1>Thanks for joining!</h1>
+    <p>We’ll keep you posted as RentMadeEasy rolls out.</p>
+    <a href="/" style="color:#0fa3b1;text-decoration:none;">← Back to home</a>
+  </main>
+</template>
